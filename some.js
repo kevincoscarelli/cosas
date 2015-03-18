20c02f707251589d00197c1176da23890c523843
unknown <kevin.coscarelli@GLB-D0614.globant.com>
2015-03-18 17:12:06 -0300

    bootcamp, topic 1, practice 7 terminado, Modularidad añadida, todo va de acuerdo al flan

--- some.js
+++ some.js
@@ -1,28 +1,74 @@
-var hid = $('#hidden');
-var evento = document.createEvent('Event');
-var alias = $('.alias');
-var boton = $('#boton');
-var req = new XMLHttpRequest();
 
-evento.initEvent('eventox', true, true);
+function insertAfter(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
 
-document.addEventListener('eventox', function(){
-	alias.focus();
-}, false)
+//MÓDULO MANEJANDO TODO LO RELATIVO A #BOTON
 
-$(document.body).scroll(function () {
-	hid.css('bottom', toString(document.body.scrollTop + 200)+'px');
-});
+var relBoton = (function () {
+	var boton = $('#boton');
+	var req = new XMLHttpRequest();
 
-$(document.body).ready(function () {
-	setTimeout(function(){hid.fadeIn(3000, function(){
-		document.dispatchEvent(evento);
+	var priv_eval = function (elemento) {
+		var obj = JSON.parse(req.responseText);
+		var nod = document.createElement('P');
+		for (p in obj) {
+			nod.innerHTML = obj[p];
+		}
+		nod.innerHTML += "<br>" + "Now, go fuck yourself!";
+		nod.style.display = "none";
+		nod.style.top = nod.style.top + 325+ "px";
+		nod.style['font-size'] = 36 + 'px';
+		nod.className = "biggie";
+		console.log(nod.innerHTML);
+		document.body.appendChild(nod, elemento);
 
-	});}, 500);
-});
+		return nod;
+	}
+	
+	return {
+		click: function (elemento, adress) {
+				boton.click(function () {
+				req.open("GET", adress, false);
+				req.send(null);
+				console.log(req.responseText);
+				var caru = $(priv_eval(elemento));
+				setTimeout(function () {
+					caru.fadeIn(3000);
+				}, 500);
+			})
+		}
+	}
+})();
 
-boton.click(function () {
-	req.open("GET", "http://bootcamp.aws.af.cm/welcome/yourname", false);
-	req.send(null);
-	console.log(req.responseText);
-});
+
+
+//MÓDULO MANEJANDO TODO LO RELATIVO A #HIDDEN,
+//.ALIAS Y .BIGGIE, DONDE hid Y big CORRESPONDEN
+//AL <P> QUE HACE FADEIN Y alias AL <INPUT>.
+
+var relHidAlias = (function () {
+	var big = $('.biggie');
+	var hid = $('#hidden');
+	var evento = document.createEvent('Event');
+	var alias = $('.alias');
+
+	evento.initEvent('eventox', true, true);
+
+	document.addEventListener('eventox', function(){
+		alias.focus();
+	}, false);
+
+	return {
+		fadeNfocus: $(document.body).ready(function () {
+			setTimeout(function(){hid.fadeIn(3000, function(){
+				document.dispatchEvent(evento);
+			});}, 500);
+		}),
+
+		hidden: hid
+	}
+})();
+
+relHidAlias.fadeNfocus;
+relBoton.click(relHidAlias.hidden, "http://bootcamp.aws.af.cm/welcome/yourname");
